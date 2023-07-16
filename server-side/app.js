ffd0cd229db3e2018e94a67cada9097069b4ba12
Emma Papaioannou <emmapapaioannou@outlook.com>
2023-07-16 10:20:18 +0300

    Add express server to handle OCR image processing

---
+++ server-side/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const Tesseract = require('tesseract.js');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/ocr', async (req, res) => {
+    const imageUrl = req.query.imageUrl;
+
+    if (!imageUrl) {
+        return res.status(400).json({ error: 'Missing imageUrl parameter' });
+    }
+
+    try {
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+
+        const result = await Tesseract.recognize(imageBuffer, 'eng', { logger: m => console.log(m) });
+        return res.json({ text: result.data.text });
+    } catch (error) {
+        console.error('Error performing OCR:', error);
+        return res.status(500).json({ error: 'Error performing OCR' });
+    }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
