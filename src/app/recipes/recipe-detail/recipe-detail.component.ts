4b2353679e11d6bc3eebfe056e7e7ef26b84c68b
Emma Papaioannou <emmapapaioannou@outlook.com>
2023-08-05 16:46:25 +0300

    Refactor pdf print and fix recipe details update on changes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,15 +6,14 @@ import { Recipe } from '../recipe.model';
 import { RecipeService } from '../../services/recipe.service';
 import { Subscription } from 'rxjs';
 
-import { OpenSans } from 'src/app/shared/open-sans-font';
-import jsPDF from 'jspdf';
-import autotable from 'jspdf-autotable';
-import Swal from 'sweetalert2';
 import { DataStorageService } from '../../services/data-storage.service';
 import { ShoppingListService } from '../../services/shopping-list.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RecipeEditComponent } from '../recipe-edit/recipe-edit.component';
 import { AlertService } from 'src/app/services/alert.service';
+import { PdfService } from 'src/app/services/pdf.service';
+import { fontName, fontStyle } from 'src/app/shared/fonts.model';
+import { PdfOptions } from 'src/app/shared/pdfOptions.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -24,20 +23,21 @@ import { AlertService } from 'src/app/services/alert.service';
 export class RecipeDetailComponent implements OnInit, OnDestroy {
   @Output() addToShoppingListEvent = new EventEmitter<Ingredient[]>();
   @ViewChild('recipeImage') recipeImage: ElementRef;
-  recipe: Recipe = null;
+  recipe: Recipe;
   id: string;
   recipesEmpty: boolean = false;
   private paramsSubscription: Subscription;
   private editModeSubscription: Subscription;
 
   constructor(
-    public recipeService: RecipeService,
+    private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router,
     private dataStorageService: DataStorageService,
     private modalService: NgbModal,
     private alertService: AlertService,
-    private shoppingListService: ShoppingListService
+    private shoppingListService: ShoppingListService,
+    private pdfService: PdfService
   ) { }
 
   ngOnInit(): void {
@@ -45,12 +45,13 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
     this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
       this.recipe = this.recipeService.getRecipe(this.id);
+      if (!this.recipe) {
+        this.handleRefresh();
+      }
     });
 
-    this.recipeService.recipeUpdated.subscribe((id: string) => {
-      if (id === this.id) {
-        this.recipe = this.recipeService.getRecipe(this.id);
-      }
+    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+      this.recipe = this.recipeService.getRecipe(this.id);
     });
   }
 
@@ -74,8 +75,6 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
     });
   }
   
-  
-
   onEditRecipe() {
     this.recipeService.setRecipeEditMode(true);
     const modalRef = this.modalService.open(RecipeEditComponent, { backdrop: 'static', size: 'lg' });
@@ -83,80 +82,19 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
   }
 
   printRecipe() {
-    const doc = new jsPDF();
-
-    // Set constants
-    const leftMargin = 15;
-    const fontSize = 14;
-    const imageWidth = 100;
-    const imageHeight = 100;
-    const marginTop = 10;
-    const maxWidth = 180;
-
-    // Enable automatic page creation
-    // Add the Open Sans variable font
-    doc.addFileToVFS(OpenSans.name, OpenSans.base64);
-    doc.addFont(OpenSans.name, 'OpenSans', 'normal');
-    doc.addFileToVFS(OpenSans.boldName, OpenSans.boldBase64);
-    doc.addFont(OpenSans.boldName, 'OpenSans', 'bold');
-
-    // Set the Open Sans font and size
-    doc.setFontSize(fontSize);
-    doc.setFont('OpenSans');
-
-    let currentY = 10;
-
-    // Add recipe name label
-    doc.setFont('OpenSans', 'bold');
-    doc.text('Recipe Name:', leftMargin, currentY);
-    currentY += 5;
-
-    // Add recipe name value
-    doc.setFont('OpenSans', 'normal');
-    const recipeNameText = doc.splitTextToSize(this.recipe.name, maxWidth);
-    doc.text(recipeNameText, leftMargin, currentY);
-    currentY += 5 * recipeNameText.length + marginTop;
-
-    // Add recipe description label
-    doc.setFont('OpenSans', 'bold');
-    doc.text('Description:', leftMargin, currentY);
-    currentY += 5;
-
-    // Add recipe description value
-    doc.setFont('OpenSans', 'normal');
-    const recipeDescriptionText = doc.splitTextToSize(this.recipe.description, maxWidth);
-    doc.text(recipeDescriptionText, leftMargin, currentY);
-    currentY += 5 + recipeDescriptionText.length + marginTop;
-
-    // Add recipe image
-    try {
-      const imgData = this.recipe.imagePath;
-      doc.addImage(imgData, 'PNG', leftMargin, currentY, imageWidth, imageHeight);
-    } catch (error) {
-      const imgData = 'assets/images/no-photo-available2.png';
-      doc.addImage(imgData, 'PNG', leftMargin, currentY, imageWidth, imageHeight);
-      console.log(error.message)
-    }
-
-    currentY += imageHeight + marginTop;
-
-    // Take table ingredients from the html directly
-    autotable(doc, {
-      html: '#ingredients',
-      startY: currentY,
-      margin: { left: leftMargin },
-      headStyles: {
-        font: 'OpenSans',
-        fontStyle: 'bold',
-        fillColor: [184, 141, 141],
-        textColor: 'black'
-      },
-      styles: {
-      },
-    });
-
-    // Save the PDF
-    doc.save(`${this.recipe.name}.pdf`);
+    const pdfOptions: PdfOptions = {
+      leftMargin : 15, 
+      imageWidth : 100, 
+      imageHeight : 100, 
+      marginTop : 10, 
+      maxWidth : 180, 
+      fontSize : 14, 
+      fontName : fontName.helvetica, 
+      fontStyle : fontStyle.normal, 
+      titlesFontStyle : fontStyle.bold
+    };
+      
+    this.pdfService.generateRecipePdf(this.recipe, pdfOptions);
   }
 
   async onDeleteRecipe() {
@@ -188,6 +126,18 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
       }
     });
     this.router.navigate(['/recipes']);
+  }
+
+  handleRefresh() {
+    this.dataStorageService.fetchRecipes().subscribe({
+      next: (res: Recipe[]) => {
+        this.recipeService.setRecipes(res);
+        this.recipe = this.recipeService.getRecipe(this.id);
+      },
+      error: (error) => {
+        this.alertService.infoMessage(false, 'Failed to load recipe. Please reaload the page. ' + error.message);
+      }
+    });
   }
 }
 
