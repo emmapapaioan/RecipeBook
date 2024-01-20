9c17d42918ad036d52dce2503c6cb89d0cfd3df9
Emma Papaioannou <emmapapaioannou@outlook.com>
2024-01-20 14:11:25 +0200

    Add logout message

--- src/app/_services/alert.service.ts
+++ src/app/_services/alert.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import Swal, { SweetAlertIcon, SweetAlertResult } from 'sweetalert2';
+import { AuthorizationService } from './authorization.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
 
-  constructor() { }
+  constructor(private authService: AuthorizationService, private router: Router) { }
 
   infoMessage(isSuccess: boolean, text: string) {
     isSuccess ? this.successMessage(text) : this.errorMessage(text);
@@ -43,6 +45,29 @@ export class AlertService {
       icon:'error',
       title: 'Error',
       text: text
+    });
+  }
+
+  onLogoutMessage() {
+    Swal.fire({
+      text: 'Are you sure you want to log out? We\'\ll miss you!',
+      imageUrl: '/assets/icons/sad-emoji.png',
+      imageWidth: 100,
+      imageHeight: 100,
+      imageAlt: 'Sad Emoji',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, logout',
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#28a745',
+      cancelButtonColor: '#dc3545',
+      customClass: {
+        image: 'custom-image-class' 
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.authService.logout();
+        this.router.navigate(['/home']);
+      }
     });
   }
 }
