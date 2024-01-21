4d37608cc3def1459abfe3962949ec92d039b17c
Emma Papaioannou <emmapapaioannou@outlook.com>
2024-01-21 16:22:30 +0200

    Update FirebaseStorageService

--- src/app/_services/firebase-storage.service.ts
+++ src/app/_services/firebase-storage.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +10,24 @@ export class FirebaseStorageService {
   constructor(private http: HttpClient) { }
 
   uploadImageAndGetUrl(file: File): Promise<string> {
+    const uniqueFileName = `${uuidv4()}.${this.getFileExtension(file.name)}`;
     const formData = new FormData();
-    formData.append('file', file, file.name);
+    formData.append('file', file, uniqueFileName);
   
-    const uploadUrl = `https://firebasestorage.googleapis.com/v0/b/recipe-book-41dd4.appspot.com/o?uploadType=media&name=images/${encodeURIComponent(file.name)}`;
+    const uploadUrl = `https://firebasestorage.googleapis.com/v0/b/recipe-book-41dd4.appspot.com/o?uploadType=media&name=images/${encodeURIComponent(uniqueFileName)}`;
   
-    return this.http.post<any>(uploadUrl, formData)
-      .toPromise()
+    return firstValueFrom(this.http.post<any>(uploadUrl, formData))
       .then(response => {
         const fileName = encodeURIComponent(response['name']);
         return `https://firebasestorage.googleapis.com/v0/b/recipe-book-41dd4.appspot.com/o/${fileName}?alt=media`;
       });
+  }
+
+  getImageAsBlob(imageUrl: string): Promise<Blob> {
+    return firstValueFrom(this.http.get(imageUrl, { responseType: 'blob' }));
+  }
+
+  getFileExtension(filename: string): string {
+    return filename.slice(((filename.lastIndexOf(".") - 1) >>> 0) + 2);
   }
 }
