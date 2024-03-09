f49c9589f0c44a15aaa01db93c0636428c495d62
emmapapaioan <emmapapaioannou@outlook.com>
2024-03-09 18:34:19 +0200

    Add image preview when image is uploaded

--- src/app/drop-zone/drop-zone.component.ts
+++ src/app/drop-zone/drop-zone.component.ts
@@ -9,10 +9,12 @@ export class DropZoneComponent {
   @Output() fileDropped = new EventEmitter<File>();
   hovering: boolean= false;
   file: File;
+  imagePreviewUrl: string | ArrayBuffer = '';
 
   onFileDropped(files: File[]) {
     this.file = files[0];
     this.fileDropped.emit(this.file);
+    this.generateImagePreview(this.file);
   }
 
   onFileHovered(isHovering: boolean) {
@@ -22,9 +24,21 @@ export class DropZoneComponent {
   onFileSelected(event: any) {
     this.file = event.target.files[0];
     this.fileDropped.emit(this.file);
+    this.generateImagePreview(this.file);
   }
 
   removeFile() {
     this.file = null;
+    this.imagePreviewUrl = '';
+  }
+
+  generateImagePreview(file: File) {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.imagePreviewUrl = event.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
   }
 }
