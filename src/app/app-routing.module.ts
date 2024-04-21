4162678969b898060923d34c9a6b8c0ed8d91139
emmapapaioan <emmapapaioannou@outlook.com>
2024-04-21 19:54:53 +0300

    Implement PreloadAllModules strategy for recipe and shopping list modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { OcrReaderComponent } from './ocr-reader/ocr-reader.component';
 import { HomeComponent } from './home/home.component';
@@ -19,6 +19,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
     onSameUrlNavigation: 'reload'
   })],
   exports: [RouterModule]
