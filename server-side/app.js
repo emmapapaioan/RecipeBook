8fd2ee7d907a15ad6a0a2d58d44f9fbf56d95730
emmapapaioan <emmapapaioannou@outlook.com>
2024-04-28 10:49:05 +0300

    Update port settings for Heroku deployment

--- server-side/app.js
+++ server-side/app.js
@@ -26,6 +26,7 @@ app.get('/ocr', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
